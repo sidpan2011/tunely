4c34705dec87b38a84905bb741e6ace5799a16aa
Sidhanth Pandey <sidhanthpandey@192.168.1.7>
2024-10-19 13:15:18 +0530

    Fix bug with text transparent

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,7 +5,7 @@ const ContactMe = () => {
   return (
     <div className=''>
         <HighLight text={"Contact Me"} extraClassName={"items-center"} />
-        <h1 className='mt-9 whitespace-pre-wrap bg-gradient-to-br bg-clip-text lg:text-5xl md:text-4xl leading-none text-center text-transparent text-black dark:text-white font-semibold tracking-tight text-balance text-3xl translate-y-[-1rem] animate-fade-in'>Get in Touch</h1>
+        <h1 className='mt-9 whitespace-pre-wrap bg-gradient-to-br bg-clip-text lg:text-5xl md:text-4xl leading-none text-center text-black dark:text-white font-semibold tracking-tight text-balance text-3xl translate-y-[-1rem] animate-fade-in'>Get in Touch</h1>
         <div className='max-w-lg text-center mx-auto mb-52 mt-3'>
             <p>Want to chat? Just shoot me a dm with a direct question on twitter and I'll respond whenever I can. I will ignore all soliciting.</p>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div>
         <HighLight text={"My Projects"} />
-        <h1 className='mt-9 whitespace-pre-wrap bg-gradient-to-br bg-clip-text lg:text-5xl md:text-4xl leading-none text-center text-transparent text-black dark:text-white font-semibold tracking-tight text-balance text-3xl translate-y-[-1rem] animate-fade-in'>Check out my latest work</h1>
+        <h1 className='mt-9 whitespace-pre-wrap bg-gradient-to-br bg-clip-text lg:text-5xl md:text-4xl leading-none text-center text-black dark:text-white font-semibold tracking-tight text-balance text-3xl translate-y-[-1rem] animate-fade-in'>Check out my latest work</h1>
         {/* This will contain the projects */}
         <div>
 
